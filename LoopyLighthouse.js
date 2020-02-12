177d52cd190d175643df0be04f3b292425a9999f
Melis Cagan <meliscagan@gmail.com>
2020-02-12 22:33:41 +0000

    turned for loop into a function

--- LoopyLighthouse.js
+++ LoopyLighthouse.js
@@ -1,10 +1,15 @@
-for(i=100; i <=200; i++){
-  if(i%3===0 && i%4===0){
-    console.log("LoopyLighthouse");
-  } else if(i%4===0){
-    console.log("Lighthouse");
-  } else if(i%3===0){
-    console.log("Loopy");
-  }else{
-  console.log(i)}
+function loopyLighthouse([start, end], [num1, num2],[word1, word2]){
+  for(let i = start; i <= end; i++){
+    if(i % num1 ===  0 && i % num2 === 0){
+      console.log(word1 + word2);
+    } else if(i % num1 === 0){
+      console.log(word1);
+    } else if(i % num2 === 0){
+      console.log(word2);
+    } else {
+      console.log(i)
+    }
+  }
 }
+
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]))
